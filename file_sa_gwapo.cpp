116e2b71d51bec1c020ddeab0eea57f26107b05b
herogoopio-cpu <herogoopio@gmail.com>
2026-01-29 21:58:02 +0800

    Simplify number checking by removing loop
    Removed the loop for checking multiple numbers and simplified user input.

--- file_sa_gwapo.cpp
+++ file_sa_gwapo.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
-
+using namespace std;
 
 int main() {
     int number;
-    char choice;
 
-    do {
-        cout << "\nEnter a number: ";
-        cin >> number;
+    cout << "Enter a number: ";
+    cin >> number;
 
-        if (number % 2 == 0)
-            cout << number << " is EVEN." << endl;
-        else
-            cout << number << " is ODD." << endl;
+    if (number % 2 == 0)
+        cout << number << " is EVEN." << endl;
+    else
+        cout << number << " is ODD." << endl;
 
-        cout << "Check another number? (y/n): ";
-        cin >> choice;
-
-    } while (choice == 'y' || choice == 'Y');
-
-    cout << "Program ended.\n";
     return 0;
 }
