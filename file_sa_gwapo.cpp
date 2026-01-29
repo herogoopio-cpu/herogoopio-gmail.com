49a0b1f8cd661b6af2df376f7c94e6feb3c8bdeb
herogoopio-cpu <herogoopio@gmail.com>
2026-01-29 21:33:44 +0800

    Implement loop for multiple number checks
    Added a loop to allow checking multiple numbers and a choice prompt.

--- file_sa_gwapo.cpp
+++ file_sa_gwapo.cpp
@@ -1,17 +1,24 @@
-
 #include <iostream>
 using namespace std;
 
 int main() {
     int number;
+    char choice;
 
-    cout << "Enter a number: ";
-    cin >> number;
+    do {
+        cout << "\nEnter a number: ";
+        cin >> number;
 
-    if (number % 2 == 0)
-        cout << number << " is EVEN." << endl;
-    else
-        cout << number << " is ODD." << endl;
+        if (number % 2 == 0)
+            cout << number << " is EVEN." << endl;
+        else
+            cout << number << " is ODD." << endl;
 
+        cout << "Check another number? (y/n): ";
+        cin >> choice;
+
+    } while (choice == 'y' || choice == 'Y');
+
+    cout << "Program ended.\n";
     return 0;
 }
